6b34631bb7d6d672d6bf66a21cd5224b6d372408
junu0810 <junu0810@hanmail.net>
2021-11-29 23:59:33 +0900

    firebase_setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/compat/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDY_L1LSv9Kmhxpc_1J-c9pNg1OaY8yEl0",
+    authDomain: "fir-75166.firebaseapp.com",
+    projectId: "fir-75166",
+    storageBucket: "fir-75166.appspot.com",
+    messagingSenderId: "452191145898",
+    appId: "1:452191145898:web:d62cc79a83229484e6a5d3"
+  };
+
+ export default firebase.initializeApp(firebaseConfig);
